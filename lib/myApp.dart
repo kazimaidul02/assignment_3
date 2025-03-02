820b66a12486ca4f1951cfdfd59c9e280d58ab23
kazimaidul02 <shehab021087@gmail.com>
2025-03-03 03:32:14 +0600

    update

---
+++ lib/myApp.dart
@@ -0,0 +1,40 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+        builder: (context,orientation,deviceType) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            useInheritedMediaQuery: true,
+            locale: DevicePreview.locale(context),
+            builder: DevicePreview.appBuilder,
+            theme: ThemeData(
+                brightness: Brightness.light,
+                primaryColor: Colors.orangeAccent,
+                appBarTheme: AppBarTheme(
+                  centerTitle: true,
+                  color: Colors.orangeAccent,
+                ),
+                //scaffoldBackgroundColor: Colors.white,
+                elevatedButtonTheme: ElevatedButtonThemeData(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blueAccent,
+                        foregroundColor: Colors.white))),
+            title: "Ajnu",
+            home:Scaffold(
+              appBar: AppBar(
+                title: Text('kazii'),
+              ),
+            ) ,
+          );
+        }
+    );
+  }
+}
